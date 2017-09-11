324a5e5e30edd30211dfd1bf8af52f91c46daa5d
Bill Barnes <bill@billba-mbp.local>
2017-09-11 12:20:10 +1000

    Sample code

--- src/bot.ts
+++ src/bot.ts
@@ -15,8 +15,50 @@ server.listen(process.env['port'] || process.env['PORT'] || 3978, '::', () =>
 );
 server.post('/api/messages', connector.listen());
 
-const bot = new builder.UniversalBot(connector);
-
-bot.dialog('/', (session) => {
-    session.send("Hello, World");
+const bot = new builder.UniversalBot(connector, (session) => {
+    session.send("Sorry, I didn't understand that.")
 });
+
+bot.dialog('/helloworld', (session) => {
+    session.send("Hello, World");
+})
+.triggerAction({
+    matches: /^hello$/i
+});
+
+bot.dialog('/extract', (session, args) => {
+    const place = args.intent.matched[1].trim();
+    session.send(`Wherever you go, ${place} is in your heart`)
+})
+.triggerAction({
+    matches: /^where is (.*)$/i
+});
+
+bot.dialog('/list', (session, args) => {
+    const status = args.intent.matched[1].trim();
+    const results = tests.filter(test => test.status === status);
+    session.send(results.map(tests => tests.name).join(", "));
+})
+.triggerAction({
+    matches: /^which tests (failed|passed) today$/i
+});
+
+interface Test {
+    name: string;
+    status: "failed" | "passed";
+}
+
+const tests: Test[] = [{
+    name: "Dependency Injection",
+    status: "failed"
+}, {
+    name: "Input/Output",
+    status: "passed"
+}, {
+    name: "Database",
+    status: "failed"
+}, {
+    name: "Server",
+    status: "passed"
+}];
+
